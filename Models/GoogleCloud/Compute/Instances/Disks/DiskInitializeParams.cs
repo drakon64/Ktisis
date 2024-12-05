a98e027d32c565f6a7f273f214fcdd5982630c09
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-05 13:14:30 +0000

    Remove some initializers

--- Models/GoogleCloud/Compute/Instances/Disks/DiskInitializeParams.cs
+++ Models/GoogleCloud/Compute/Instances/Disks/DiskInitializeParams.cs
@@ -4,7 +4,8 @@ internal class DiskInitializeParams
 {
     public required string SourceImage { get; init; }
     public required string DiskSizeGb { get; init; }
-    public required string DiskType { get; init; }
-    public required string ProvisionedIops { get; init; }
-    public required string ProvisionedThroughput { get; init; }
+    public string DiskType =
+        $"projects/{Program.Project}/zones/{Program.Zone}/diskTypes/hyperdisk-balanced";
+    public string ProvisionedIops = "3000";
+    public string ProvisionedThroughput = "140";
 }
