3e570b138f869e53da3e3f3561548185a2ee918e
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-21 23:49:21 +0000

    Remove setters for `NetworkInterface`

--- Ktisis.Common/Models/GoogleCloud/Compute/Instances/NetworkInterfaces/NetworkInterface.cs
+++ Ktisis.Common/Models/GoogleCloud/Compute/Instances/NetworkInterfaces/NetworkInterface.cs
@@ -2,8 +2,8 @@ namespace Ktisis.Common.Models.GoogleCloud.Compute.Instances.NetworkInterfaces;
 
 public class NetworkInterface
 {
-    public string? Network { get; set; }
-    public string? Subnetwork { get; set; }
+    public string? Network { get; init; }
+    public string? Subnetwork { get; init; }
     public readonly NetworkInterfaceAccessConfig[] AccessConfigs = [new()];
     public const string NicType = "GVNIC";
 }
