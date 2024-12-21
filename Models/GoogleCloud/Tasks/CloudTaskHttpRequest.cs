5f50f38ac83492681b660c76a68aaf3de3a96f41
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-21 20:37:21 +0000

    Formatting

--- Models/GoogleCloud/Tasks/CloudTaskHttpRequest.cs
+++ Models/GoogleCloud/Tasks/CloudTaskHttpRequest.cs
@@ -6,7 +6,10 @@ internal class CloudTaskHttpRequest(string body)
 {
     public required string Url { get; init; }
     public const string HttpMethod = "POST";
-    public readonly Dictionary<string, string> Headers = new() { { "Content-Type", "application/json" } };
+    public readonly Dictionary<string, string> Headers = new()
+    {
+        { "Content-Type", "application/json" },
+    };
     public readonly string Body = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(body));
 
     [JsonPropertyName("oauthToken")]
