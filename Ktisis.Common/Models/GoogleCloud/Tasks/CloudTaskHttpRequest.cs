5a3dc135016905a9729885759844af855f3f8441
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-24 16:53:15 +0000

    Disable "Convert into const" inspection

--- Ktisis.Common/Models/GoogleCloud/Tasks/CloudTaskHttpRequest.cs
+++ Ktisis.Common/Models/GoogleCloud/Tasks/CloudTaskHttpRequest.cs
@@ -5,7 +5,11 @@ namespace Ktisis.Common.Models.GoogleCloud.Tasks;
 public class CloudTaskHttpRequest
 {
     public required string Url { get; init; }
+
+    // `const` doesn't work well with (de)serialization
+    // ReSharper disable once ConvertToConstant.Global
     public readonly string HttpMethod = "POST";
+
     public readonly ReadOnlyDictionary<string, string> Headers = new Dictionary<string, string>
     {
         { "Content-Type", "application/json" },
