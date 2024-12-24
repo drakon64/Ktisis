7850bf8f0895ccaa44818f2a7ad7e4d520010e15
Adam Chance <6444703+drakon64@users.noreply.github.com>
2024-12-24 16:47:24 +0000

    Store headers in a read-only dictionary

--- Ktisis.Common/Models/GoogleCloud/Tasks/CloudTaskHttpRequest.cs
+++ Ktisis.Common/Models/GoogleCloud/Tasks/CloudTaskHttpRequest.cs
@@ -1,12 +1,14 @@
+using System.Collections.ObjectModel;
+
 namespace Ktisis.Common.Models.GoogleCloud.Tasks;
 
 public class CloudTaskHttpRequest
 {
     public required string Url { get; init; }
     public readonly string HttpMethod = "POST";
-    public readonly Dictionary<string, string> Headers = new()
+    public readonly ReadOnlyDictionary<string, string> Headers = new Dictionary<string, string>
     {
         { "Content-Type", "application/json" },
-    };
+    }.AsReadOnly();
     public required string Body { get; init; }
 }
