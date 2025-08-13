6ad73d8524ee5a9b152a51d6071223c65278eb0c
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-08-13 20:22:42 +0100

    Command-line arguments

--- Package.swift
+++ Package.swift
@@ -4,11 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Ktisis",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "Ktisis"),
-    ]
+  name: "Ktisis",
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.6.1")),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "Ktisis",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ])
+  ]
 )
