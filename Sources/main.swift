bcfe99213abd25d2ec997f41a780019f199a16ef
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-08-13 21:53:52 +0100

    Command-line options for organisations and repositories

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,6 +11,12 @@ struct Ktisis: AsyncParsableCommand {
   @Flag(help: "Run the Processor job.")
   var processor = false
 
+  @Option(name: .shortAndLong, help: "The GitHub organisations to allow requests from.")
+  var organisations: [String] = []
+
+  @Option(name: .shortAndLong, help: "The GitHub repositories to allow requests from.")
+  var repositories: [String] = []
+
   mutating func run() async throws {
     if processor {
 
