326b16d51df3b04064039a72569a53e1185be5a4
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-09-17 01:27:28 +0100

    Fix .NET runtime

--- default.nix
+++ default.nix
@@ -30,7 +30,7 @@ rec {
       nugetDeps = ./deps.json;
 
       dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
-      dotnet-runtime = pkgs.dotnetCorePackages.runtime_9_0;
+      dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_9_0;
 
       executables = [ "Ktisis" ];
     };
