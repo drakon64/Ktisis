234ff6ae063df80c85b93d3c5daf67e8ea3000b2
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-09-28 16:20:38 +0100

    (Temp) Log Cloud Tasks payload JSON

--- src/Clients/CloudTasksClient.cs
+++ src/Clients/CloudTasksClient.cs
@@ -10,6 +10,17 @@ internal static class CloudTasksClient
     {
         var token = await GoogleCloudClient.RefreshAccessToken();
 
+        var task = new CreateCloudTask
+        {
+            Task = new CloudTask
+            {
+                Name = $"{Program.Queue}/tasks/test",
+                HttpRequest = new HttpRequest(repository),
+            },
+        };
+
+        Console.WriteLine(JsonSerializer.Serialize(task));
+
         return await Program.HttpClient.SendAsync(
             new HttpRequestMessage
             {
@@ -17,16 +28,7 @@ internal static class CloudTasksClient
                 Headers = { { "Authorization", $"{token.TokenType} {token.AccessToken}" } },
                 Method = HttpMethod.Post,
 
-                Content = JsonContent.Create(
-                    new CreateCloudTask
-                    {
-                        Task = new CloudTask
-                        {
-                            Name = $"{Program.Queue}/tasks/test",
-                            HttpRequest = new HttpRequest(repository),
-                        },
-                    }
-                ),
+                Content = JsonContent.Create(task),
             }
         );
     }
