5aef90ec50c858579a825cd4560fc5b9ef1d9b6e
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-09-28 20:27:33 +0100

    Support generating GitHub Actions runner registration tokens

--- src/Clients/GitHubClient.cs
+++ src/Clients/GitHubClient.cs
@@ -88,9 +88,41 @@ internal static class GitHubClient
         )!;
     }
 
+    public static async Task<string> CreateRunnerRegistrationToken(
+        string repo,
+        ulong installationId
+    )
+    {
+        await GetGitHubInstallationAccessToken(installationId);
+
+        var request = await Program.HttpClient.SendAsync(
+            new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                Headers =
+                {
+                    { "Authorization", $"Bearer {_githubInstallationAccessToken.Token}" },
+                    { "User-Agent", "Ktisis/0.0.1" },
+                    { "Accept", "application/vnd.github+json" },
+                    { "X-GitHub-Api-Version", "2022-11-28" },
+                },
+                RequestUri = new Uri(
+                    $"https://api.github.com/repos/{repo}/actions/runners/registration-token"
+                ),
+            }
+        );
+
+        return (await request.Content.ReadFromJsonAsync<RunnerRegistrationToken>())!.Token;
+    }
+
     private class InstallationAccessToken
     {
         public required string Token { get; init; }
         public required DateTime ExpiresAt { get; init; }
+    }
+
+    private class RunnerRegistrationToken
+    {
+        public required string Token { get; init; }
     }
 }
