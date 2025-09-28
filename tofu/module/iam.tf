514e7b8db96795a965215d3788a1f259d4c20b34
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-09-28 16:38:49 +0100

    Allow Receiver to act as itself

--- tofu/module/iam.tf
+++ tofu/module/iam.tf
@@ -4,6 +4,12 @@ resource "google_service_account" "ktisis" {
   account_id = "ktisis-${each.key}"
 }
 
+resource "google_service_account_iam_member" "iam" {
+  member             = google_service_account.ktisis["receiver"].member
+  role               = "roles/iam.serviceAccountUser"
+  service_account_id = google_service_account.ktisis["receiver"].id
+}
+
 resource "google_project_iam_member" "compute" {
   member  = google_service_account.ktisis["processor"].member
   project = data.google_project.project.project_id
