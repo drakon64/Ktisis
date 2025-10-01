bdef565aaa19764c4c2497eac25154163649c287
NotKeira <github.rxs06@accounts.keira.boo>
2025-10-01 23:02:07 +0100

    feat: make it better

--- src/Program.cs
+++ src/Program.cs
@@ -10,29 +10,31 @@ public static class Program
     internal static readonly string[]? Repositories = Environment
         .GetEnvironmentVariable("KTISIS_GITHUB_REPOSITORIES")
         ?.Split(" ");
-
+    
     internal static readonly HttpClient HttpClient = new();
 
     public static void Main()
     {
         var builder = WebApplication.CreateSlimBuilder();
         builder.Services.AddSingleton<WebhookEventProcessor, WorkflowJobWebhookEventProcessor>();
-
+        
         var app = builder.Build();
 
-        // Receiver
+        // ✨ webhook receiver uwu ✨
         app.MapGitHubWebhooks(
             secret: Environment.GetEnvironmentVariable("KTISIS_GITHUB_WEBHOOK_SECRET")
         );
 
-        // Processor
+        // 💖 processor time!! 💖
         app.MapPost(
             "/api/ktisis",
             async (string name, string repository, long installationId) =>
                 await ComputeEngineClient.CreateInstance(name, repository, installationId)
         );
-        app.MapDelete("/api/ktisis", () => "Hello World!");
 
+        app.MapDelete("/api/ktisis", () => "byeee~ 💕");
+
+        // 🌸 lets goooo 🌸
         app.Run($"http://*:{Environment.GetEnvironmentVariable("PORT")}");
     }
 }
