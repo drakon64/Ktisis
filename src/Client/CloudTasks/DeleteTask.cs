96eec8550facd067e7f43039316561050f43e502
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-08 23:13:22 +0100

    Fix deleting Cloud Tasks

--- src/Client/CloudTasks/DeleteTask.cs
+++ src/Client/CloudTasks/DeleteTask.cs
@@ -1,3 +1,6 @@
+using System.IO.Hashing;
+using System.Text;
+
 namespace Ktisis.Client.CloudTasks;
 
 internal static partial class CloudTasksClient
@@ -9,13 +12,15 @@ internal static partial class CloudTasksClient
         long jobId
     )
     {
-        var workflowJob = GetWorkflowJob(repository, runId, jobId);
+        var taskName = Convert.ToHexStringLower(
+            XxHash3.Hash(Encoding.Default.GetBytes("c-" + GetWorkflowJob(repository, runId, jobId)))
+        );
 
         return await Program.HttpClient.SendAsync(
             new HttpRequestMessage
             {
                 RequestUri = new Uri(
-                    $"https://cloudtasks.googleapis.com/v2/{Queue}/tasks/c-{workflowJob}"
+                    $"https://cloudtasks.googleapis.com/v2/{Queue}/tasks/{taskName}"
                 ),
                 Headers = { { "Authorization", await GoogleCloudClient.GetAccessToken() } },
                 Method = HttpMethod.Delete,
