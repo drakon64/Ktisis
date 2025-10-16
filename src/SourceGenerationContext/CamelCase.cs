818ee3427f8202dc2aca4ef46e70cf821ee3fe43
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-16 20:51:34 +0100

    Remove redundant source generator

--- src/SourceGenerationContext/CamelCase.cs
+++ src/SourceGenerationContext/CamelCase.cs
@@ -1,12 +1,10 @@
 using System.Text.Json.Serialization;
-using Ktisis.Client;
 using Ktisis.Client.CloudTasks;
 using Ktisis.Client.ComputeEngine;
 
 namespace Ktisis.SourceGenerationContext;
 
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
-[JsonSerializable(typeof(GoogleCloudClient.AccessTokenResponse))]
 [JsonSerializable(typeof(CloudTasksClient.TaskRequest))]
 [JsonSerializable(typeof(ComputeEngineClient.CreateInstanceRequest))]
 [JsonSerializable(typeof(ComputeEngineClient.InstanceTemplate))]
