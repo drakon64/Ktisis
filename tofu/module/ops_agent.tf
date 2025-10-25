5635ba0fa33f470d9c04b69b2decd6cf9110c365
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-10-25 17:26:57 +0100

    Install Google Ops Agent with a policy

---
+++ tofu/module/ops_agent.tf
@@ -0,0 +1,19 @@
+module "ops_agent_policy" {
+  for_each = var.zones
+
+  source  = "terraform-google-modules/cloud-operations/google//modules/ops-agent-policy"
+  version = "~> 0"
+  
+  project       = data.google_project.project.id
+  zone          = each.value
+  assignment_id = "ktisis-ops-agent-policy"
+
+  agents_rule = {
+    package_state = "installed"
+    version       = "latest"
+  }
+
+  instance_filter = {
+    all = true
+  }
+}
