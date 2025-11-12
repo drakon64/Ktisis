64deec6a7e41fad41d51b870ea2f905e8cdd568b
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-11-12 11:44:09 +0000

    Evaluate Nix with `nix-eval-jobs`

--- shell.nix
+++ shell.nix
@@ -7,12 +7,17 @@ pkgs.mkShellNoCC {
     graphviz
     lon
     nixfmt-rfc-style
+    nix-eval-jobs
     opentofu
   ];
 
   passthru = {
     lon = pkgs.mkShellNoCC {
       packages = [ pkgs.lon ];
+    };
+
+    nix-eval-jobs = pkgs.mkShellNoCC {
+      packages = [ pkgs.nix-eval-jobs ];
     };
 
     opentofu = pkgs.mkShellNoCC {
