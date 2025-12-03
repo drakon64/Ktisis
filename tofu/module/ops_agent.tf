b10aa2b0f819cd5f2f920baa38358ee99200ae12
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2025-12-03 18:36:09 +0000

    Formatting

--- tofu/module/ops_agent.tf
+++ tofu/module/ops_agent.tf
@@ -3,7 +3,7 @@ module "ops_agent_policy" {
 
   source  = "terraform-google-modules/cloud-operations/google//modules/ops-agent-policy"
   version = "0.6.0"
-  
+
   project       = data.google_project.project.id
   zone          = each.value
   assignment_id = "ktisis-ops-agent-policy"
