feaeec98430723754ca70586ca9f331bb4f81e90
Evelyn Chance <6444703+drakon64@users.noreply.github.com>
2026-05-19 19:27:36 +0100

    Formatting

--- src/Webhook/Program.cs
+++ src/Webhook/Program.cs
@@ -11,7 +11,10 @@ public class Program
 
         builder.Services.ConfigureHttpJsonOptions(options =>
         {
-            options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+            options.SerializerOptions.TypeInfoResolverChain.Insert(
+                0,
+                AppJsonSerializerContext.Default
+            );
         });
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -30,17 +33,20 @@ public class Program
             new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
             new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
             new(4, "Clean the bathroom"),
-            new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+            new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2))),
         ];
 
         var todosApi = app.MapGroup("/todos");
-        todosApi.MapGet("/", () => sampleTodos)
-                .WithName("GetTodos");
+        todosApi.MapGet("/", () => sampleTodos).WithName("GetTodos");
 
-        todosApi.MapGet("/{id}", Results<Ok<Todo>, NotFound> (int id) =>
-            sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-                ? TypedResults.Ok(todo)
-                : TypedResults.NotFound())
+        todosApi
+            .MapGet(
+                "/{id}",
+                Results<Ok<Todo>, NotFound> (int id) =>
+                    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+                        ? TypedResults.Ok(todo)
+                        : TypedResults.NotFound()
+            )
             .WithName("GetTodoById");
 
         app.Run();
@@ -50,7 +56,4 @@ public class Program
 public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
 
 [JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
-}
+internal partial class AppJsonSerializerContext : JsonSerializerContext { }
